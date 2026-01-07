36a07834bbbd7be1bdac6a5f6f15f5632bcb4915
ruskeyz <lutayilya@gmail.com>
2026-01-07 23:33:53 +0000

    feat: Creating groups.

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -17,8 +20,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         delta_time = clock.tick(60)
         dt = delta_time / 1000
